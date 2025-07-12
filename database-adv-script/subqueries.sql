cec8c26ef7c2c736de2fc841ab5d88571575375b
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-07-12 15:10:53 +0300

    m

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,24 +1,10 @@
--- Retrieve all bookings and the respective users who made those bookings
-SELECT b.*, u.*
-FROM bookings b
-INNER JOIN users u ON b.user_id = u.id
-ORDER BY b.id;
--- Retrieve all properties and their reviews, including properties that have no reviews
-SELECT p.*, r.*
-FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id
-ORDER BY p.id;
--- Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
-SELECT u.*, b.*
-FROM users u
-FULL OUTER JOIN bookings b ON u.id = b.user_id
-ORDER BY u.id;
--- Emulated FULL OUTER JOIN for MySQL
-SELECT u.*, b.*
-FROM users u
-LEFT JOIN bookings b ON u.id = b.user_id
-UNION
-SELECT u.*, b.*
-FROM users u
-RIGHT JOIN bookings b ON u.id = b.user_id
-ORDER BY u.id;
+-- Find all properties with average rating greater than 4.0
+SELECT *
+FROM properties
+WHERE id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+)
+ORDER BY id;
