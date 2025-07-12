c3c649ee04bd8db309a1030b68b714b57afe1c52
Brian Muuo <81385152+prey801@users.noreply.github.com>
2025-07-12 15:13:23 +0300

    m

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,4 +1,15 @@
--- Find users who have made more than 3 bookings
+-- Find all properties with average rating greater than 4.0
+SELECT *
+FROM properties
+WHERE id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+)
+ORDER BY id;
+
+-- Find all users who have made bookings
 SELECT *
 FROM users u
 WHERE (
